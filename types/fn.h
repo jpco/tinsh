36e0a188b8256729664094a660b023e15cc4bf97
jpco <mail@jpconger.com>
2015-07-12 09:44:25 -0700

    added fn struct

---
+++ types/fn.h
@@ -0,0 +1,13 @@
+#ifndef JPSH_TYPES_FN_H
+#define JPSH_TYPES_FN_H
+
+#include "arglist.h"
+#include "block.h"
+
+typedef struct {
+        char *name;
+        arglist_j args;
+        block_j block;
+} fn_j;
+
+#endif
